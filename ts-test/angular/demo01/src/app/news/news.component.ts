dec411770d4603abcf7679b1019df44a8dfac729
unknown <1399472680@qq.com>
2021-01-23 15:31:36 +0800

    change

--- ts-test/angular/demo01/src/app/news/news.component.ts
+++ ts-test/angular/demo01/src/app/news/news.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent {
 
-  constructor() { }
+  public title:string = '我是新闻组件'
 
-  ngOnInit(): void {
+  public tmp1:string = '<a href="http://www.baidu.com" target="_blank">百度</a>'
+
+
+  private list:string[] = ['111', '222', '333']
+
+
+  private flag:boolean = true
+
+  private redAttr:string = 'red'
+
+  public todayD:any = new Date()
+
+  constructor() {
+
+    
+  }
+
+  ngAfterContentInit(): void {
+  
   }
 
 }
